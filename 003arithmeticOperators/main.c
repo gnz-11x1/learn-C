cfe26fbf95ff6a9b5e0dc3792253ed55ec30c01e
gnz-11x1 <sayakmajumder@hotmail.com>
2025-03-06 12:52:13 +0530

    arithmeticOperators

---
+++ 003arithmeticOperators/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 10;
+    int y = 2;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+    
+    // x++; increment
+    // x--; decrement
+
+    // augmented assignment operators
+    // x+=2;
+    // x-=2;
+    // x*=2;
+    // x/=2;
+
+    printf("%d", x);
+
+    return 0;
+}
