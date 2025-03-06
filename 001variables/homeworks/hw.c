400608fa3c2d470b53eece4442a39b3119932bc2
gnz-11x1 <sayakmajumder@hotmail.com>
2025-03-06 07:49:21 +0530

    variables

---
+++ 001variables/homeworks/hw.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int noOfLoves = 100; // int data type
+    printf("I love you %d times\n", noOfLoves);
+    float noOfParallelUniverse = 3.69; // float data type
+    printf("in %f parallel universes\n", noOfParallelUniverse);
+    double withHeartBeat = 3.6969696969696969; // double data type
+    printf("with a heart beat of %lf cause I'm a alien\n", withHeartBeat);
+    char language = 'C'; // char data type
+    printf("Today is my first day of learning programming, also in %c\n", language);
+    char yesBro[] = "btw, You're doing a great job, man, I completed my hw, I don't think you'll call police anymore"; // char[] 
+    printf("%s\n", yesBro);
+    bool doWeLoveYou = true; // bool data type
+    printf("btw, yes we love you %d\n", doWeLoveYou);
+
+    return 0;
+}
